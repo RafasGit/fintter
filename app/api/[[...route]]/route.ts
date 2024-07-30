7e2c3d6fa8dcaf09e32d24dd25365a6239c315b8
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-07-30 19:04:32 +0300

    db ressurect

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { clerkMiddleware } from "@hono/clerk-auth";
 
 import accounts from "./accounts";
 import categories from "./categories";
